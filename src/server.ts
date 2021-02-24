93b4009a38a8edd04636d8b7cd93d93201538bdd
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-02-23 23:10:07 -0300

    create route for users

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
-  return response.json({message: "Hello World!"})
-})
-
-app.post('/', (request, response) => {
-  return response.json({message: 'Os dados foram salvos com sucesso!'})
-})
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
