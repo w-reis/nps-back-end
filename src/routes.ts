b9f368affa8edb965b7b707e45dd031a9d94089a
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-02-25 14:54:17 -0300

    create route to send mail

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,19 @@
-import { Router } from 'express';
-import { SurveyController } from './controllers/SurveyController';
-import { UserController } from './controllers/UserController';
+import { Router } from "express";
+import { SendMailController } from "./controllers/SendMailContoller";
+import { SurveyController } from "./controllers/SurveyController";
+import { UserController } from "./controllers/UserController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 
-router.post('/users', userController.create);
+router.post("/users", userController.create);
 
-router.post('/surveys', surveyController.create);
-router.get('/surveys', surveyController.show);
+router.post("/surveys", surveyController.create);
+router.get("/surveys", surveyController.show);
 
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
