42e8cd43206870f16080e7a1533885bf3176b7bb
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-02-25 17:26:38 -0300

    add answers links to email

--- src/controllers/SendMailContoller.ts
+++ src/controllers/SendMailContoller.ts
@@ -55,6 +55,8 @@ class SendMailController {
       name: user.name,
       title: survey.title,
       description: survey.description,
+      user_id: user.id,
+      link: process.env.URL_MAIL
     };
 
     await SendMailService.execute(email, survey.title, variables, npsPath);
