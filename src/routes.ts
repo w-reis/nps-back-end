b3179f2b4e29cae199676ff4a8cb049370f7f912
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-02-26 13:18:30 -0300

    create route to get answer

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AnswerController } from "./controllers/AnswerController";
 import { SendMailController } from "./controllers/SendMailContoller";
 import { SurveyController } from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
@@ -8,6 +9,7 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post("/users", userController.create);
 
@@ -15,5 +17,7 @@ router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answer", answerController.execute);
 
 export { router };
