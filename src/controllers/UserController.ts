9f1f39d3e03f58b7a3f2fb85e3e738bf24f3ebba
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-02-26 14:12:42 -0300

    create validation to fields

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,27 +1,39 @@
-import { Request, Response } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { UsersRepository } from '../repositories/UsersRepository';
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
 
 class UserController {
-
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
-    
-    const usersRepository = getCustomRepository(UsersRepository)
 
-    const userAlreadyExists = await usersRepository.findOne({
-      email
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
     });
 
-    if(userAlreadyExists) {
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      return response.status(400).json({ error: error });
+    }
+
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
       return response.status(400).json({
-        error: "User already exists!"
-      })
+        error: "User already exists!",
+      });
     }
 
     const user = usersRepository.create({
-      name, email
-    })
+      name,
+      email,
+    });
 
     await usersRepository.save(user);
 
